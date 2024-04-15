bfebe3eac86416e6833de153c7c5480deb02b65e
karthik.m <karthik.m@soroco.com>
2024-04-15 21:05:20 +0530

    thirsty code added

--- hungry.py
+++ hungry.py
@@ -4,4 +4,6 @@ if hungry == 'Yes':
     print("Eat Pizza")
     print("Eat somthing")
 else:
-    print("Do your work")
+    thirsty = input("Are you thirsty")
+    if thirsty == 'Yes':
+        print("Drink water")
