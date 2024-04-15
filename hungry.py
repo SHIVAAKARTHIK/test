3079f0b4081d01a657189c87317d9174d856da22
karthik.m <karthik.m@soroco.com>
2024-04-15 21:07:55 +0530

    Fries

--- hungry.py
+++ hungry.py
@@ -2,6 +2,7 @@ hungry = input("Are you hungry")
 if hungry == 'Yes':
     print("Eat Burger")
     print("Eat Pizza")
+    print("Eat Fries")
     print("Eat somthing")
 else:
     print("Do your work")
